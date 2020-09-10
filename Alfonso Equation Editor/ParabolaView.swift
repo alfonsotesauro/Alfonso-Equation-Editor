ebf30d6d78d1daf08d4deb79b3587811f0171de7
Alfonso Tesauro <alfonsotesauro@users.noreply.github.com>
2020-09-10 20:50:02 +0200

    Commit with Github Desktop. Fixes some issues.
    Commit with Github Desktop. Fixes some issues.

--- Alfonso Equation Editor/ParabolaView.swift
+++ Alfonso Equation Editor/ParabolaView.swift
@@ -1,37 +1,85 @@
 //
-//  ParabolaView.swift
+//  GeneralEquationView.swift
 //  Alfonso Equation Editor
 //
-//  Created by Alfonso Maria Tesauro on 06/12/2019.
+//  Created by Alfonso Maria Tesauro on 07/12/2019.
 //  Copyright © 2019 Alfonso Maria Tesauro. All rights reserved.
 //
 
 import Cocoa
 
-class ParabolaView: GeneralEquationView {
+let shuldDrawExactCenterRect = false
 
-   override func transformXIntoY(_ x: Double) -> Double {
+class ParabolaView: NumbersView {
+    
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        
+        // Drawing code here.
+        
+    
+        
+        
+        let distanceBetweenVerticalLines = self.distanceBetweenVerticalLines * multiplyFactor
+        
+        
+        var ctr = -Double(width / 2) // -10000 is the original
+        
+        let finalValue = fabs(ctr)
+        
+
+        if self.appDelegate!.shouldDrawParabola {
+            let currentSegment = NSBezierPath.init()
+            currentSegment.lineWidth = CGFloat(self.appDelegate.fourthSliderDoubleValue)
             
-    return x ^^ 2
+            var firstCorrectY = self.transformXIntoY(Double(ctr * distanceBetweenVerticalLines))
             
+            firstCorrectY = firstCorrectY + Double(height / 2)
+            
+            currentSegment.move(to: NSPoint.init(x: 0, y: firstCorrectY))
+            
+            repeat {
+                
+                
+                let correctX = ctr + Double(width / 2)
+                var correctY = ctr + Double(height / 2)
+                
+                if self.appDelegate.shouldApplyParabolaDivisor {
+                
+                    correctY = self.transformXIntoY(Double(ctr)) / self.steps
+                    
+                } else {
+                    correctY = self.transformXIntoY(Double(ctr))
+                }
+                
+                correctY = correctY + Double(height / 2)
+                
+                
+                
+                
+                // currentSegment.line(to: NSPoint.init(x: Int(correctX + 3.0), y: correctY))
+                currentSegment.curve(to: NSPoint.init(x: correctX, y: correctY), controlPoint1: NSPoint.init(x: correctX, y: correctY), controlPoint2: NSPoint.init(x: correctX, y: correctY))
+                
+                
+                
+                ctr += 3.0
+            } while(ctr < finalValue)
+            NSColor.red.set()
+            currentSegment.stroke()
+        }
+        
+      
+        
+      
+        
+        
+        
+    }
+    func transformXIntoY(_ x: Double) -> Double {
+        
+        return x ^^ 2
+        
     }
     
 }
-
-struct CartesianCoordinates {
-    
-    var x: CGFloat
-    var y: CGFloat
-    
-    init(cocoaCoordinatesPoint: NSPoint, inView: NSView) {
-        let viewFrame = inView.frame
-        
-        self.x = cocoaCoordinatesPoint.x + viewFrame.size.width / 2
-        self.y = cocoaCoordinatesPoint.y + viewFrame.size.height / 2
-        
-    }
-    
-    
-    
-}
-
---
+++ Alfonso Equation Editor/RettaView.swift
@@ -0,0 +1,75 @@
+//
+//  GeneralEquationView.swift
+//  Alfonso Equation Editor
+//
+//  Created by Alfonso Maria Tesauro on 07/12/2019.
+//  Copyright © 2019 Alfonso Maria Tesauro. All rights reserved.
+//
+
+import Cocoa
+
+
+class RettaView: PowerOfThreeView {
+    
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        
+        // Drawing code here.
+        
+        
+        let distanceBetweenVerticalLines = self.distanceBetweenVerticalLines * multiplyFactor
+        
+        
+        var ctr = -Double(width / 2) // -10000 is the original
+        
+        var finalValue = fabs(ctr)
+        
+        
+        if self.appDelegate!.shouldDrawRetta {
+        
+        ctr = -appDelegate!.thirdSliderDoubleValue // -10000 is the original
+        
+        finalValue = fabs(ctr)
+        
+        let currentSegment2 = NSBezierPath.init()
+        currentSegment2.lineWidth = CGFloat(self.appDelegate.fourthSliderDoubleValue)
+        var firstCorrectY2 = ctr * distanceBetweenVerticalLines
+        
+        firstCorrectY2 = firstCorrectY2 + 0 //Double(height / 2)
+        
+        currentSegment2.move(to: NSPoint.init(x: 0, y: firstCorrectY2))
+        
+        repeat {
+            
+            
+            var correctX = (ctr * distanceBetweenVerticalLines) / (self.steps / distanceBetweenVerticalLines)
+            
+            correctX = correctX + Double(width / 2)
+            
+            var correctY = (ctr * distanceBetweenVerticalLines) / (self.steps / distanceBetweenVerticalLines)
+            
+            correctY = correctY + Double(height / 2)
+            
+            
+            
+            
+            // currentSegment.line(to: NSPoint.init(x: Int(correctX + 3.0), y: correctY))
+            currentSegment2.curve(to: NSPoint.init(x: correctX, y: correctY), controlPoint1: NSPoint.init(x: correctX, y: correctY), controlPoint2: NSPoint.init(x: correctX, y: correctY))
+            
+            
+            
+            ctr += 3.0
+        } while(ctr < finalValue)
+        NSColor.blue.set()
+        currentSegment2.stroke()
+    }
+        
+     
+        
+        
+        
+    }
+   
+    
+}
